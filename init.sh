2642cd07233244c738aaf2d78bef9f85824f0894
callisto1337 <hazardous333@gmail.com>
2016-03-08 17:58:31 +0000

    added project ask

--- init.sh
+++ init.sh
@@ -3,3 +3,4 @@ sudo ln /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/nginx.conf
 sudo rm -f /etc/gunicorn.d/django.example
 sudo rm -f /etc/gunicorn.d/wsgi.example
 sudo ln /home/box/web/etc/hello.conf /etc/gunicorn.d/
+sudo ln /home/box/web/etc/ask.conf /etc/gunicorn.d/
