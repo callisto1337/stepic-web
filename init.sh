6f22dbff409ee446f6d087e6d320aa774639c8d6
callisto1337 <hazardous333@gmail.com>
2016-03-08 16:36:48 +0000

    added files

--- init.sh
+++ init.sh
@@ -1,2 +1,5 @@
 sudo rm -f /etc/nginx/sites-enabled/default
 sudo ln /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/nginx.conf
+sudo rm -f /etc/gunicorn.d/django.example
+sudo rm -f /etc/gunicorn.d/wsgi.example
+sudo ln /home/box/web/etc/hello.conf /etc/gunicorn.d/
