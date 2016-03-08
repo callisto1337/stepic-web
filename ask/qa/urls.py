744e6c5b29a831d87cd4ea08dfb56b7e765feb74
callisto1337 <hazardous333@gmail.com>
2016-03-08 19:22:18 +0000

    finished first task

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -4,5 +4,10 @@ from qa.views import test
 
 urlpatterns = patterns('qa.views',
     url(r'^$', 'test'),
-    url(r'^login/', test)
+    url(r'^login/', test),
+    url(r'^signup/', test),
+    url(r'^question/(?P<slug>\w+)/$', test),
+    url(r'^ask/', test),
+    url(r'^popular/', test),
+    url(r'^new/', test)
 )
