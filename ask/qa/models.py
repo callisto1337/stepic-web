ce4b97085ba6ea38b288db96ea601f2970edbeb4
callisto1337 <hazardous333@gmail.com>
2016-03-10 22:00:49 +0000

    new settings

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Question(models.Model):
+	title = models.CharField()
+	text = models.TextField()
+	added_at = models.DateTimeFile()
+	rating = models.IntegerField()
+	author = models.ForeignKey(User, null=True, on_delete=models.DO_NOTHING)
+	likes = models.ForeignKey(User, null=True, on_delete=models.DO_NOTHING)
+
+class Answer(models.Model):
+	text = models.models.TextField()
+	added_at = models.DateTimeFile()
+	question = models.ForeignKey(Question, null=True, on_delete=DO_NOTHING)
+	author = models.ForeignKey(User, null=True, on_delete=models.DO_NOTHING)
