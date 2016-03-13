5622341011d74616c99a549f45a8e877fbb924fb
callisto1337 <hazardous333@gmail.com>
2016-03-13 13:05:07 +0000

    fixed

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -6,13 +6,13 @@ from django.contrib.auth.models import User
 class Question(models.Model):
 	title = models.CharField(max_length=255)
 	text = models.TextField()
-	added_at = models.DateTimeField()
+	added_at = models.DateTimeField(auto_now_add=True)
 	rating = models.IntegerField()
 	author = models.ForeignKey(User, related_name='+')
 	likes = models.ForeignKey(User, related_name='+')
 
 class Answer(models.Model):
 	text = models.TextField()
-	added_at = models.DateTimeField()
+	added_at = models.DateTimeField(auto_now_add=True)
 	question = models.ForeignKey(Question)
 	author = models.ForeignKey(User)
